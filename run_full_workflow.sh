6cb8f8b9d423a7415a96202688959b6d8b7bc125
alphapept <jalew188@gmail.com>
2023-05-26 10:57:01 +0200

    init

--- run_full_workflow.sh
+++ run_full_workflow.sh
@@ -2,17 +2,20 @@
 # SEARCH_TYPE can be "merge-lib", "first-search", "second-search"
 
 #********** file paths MUST NOT contain spaces *************
-
+_wait="--wait"
 ### login hpcl: ssh -l your_user_name -X hpcl7001
-### copy the script folder to your workspace pool folder: copy -r /fs/pool/pool-mann-pub/User/diann_linux/slurm_template/* your_pool_folder
+
+### copy the script folder to your workspace pool folder: copy -r /fs/pool/pool-mann-pub/User/diann_linux/slurm_scripts/* your_pool_folder
 ### cd your_pool_folder
-### check job ids in the queue: squeue or squeue -u {user}
+
+### run jobs: sh run_full_workflow.sh
+
+### check job ids in the queue: "squeue" or "squeue -u {username}"
 ### cancel a job: scancel your_job_id
 
-_wait="--wait"
 
 
-
+### Change settings below for your project
 RAW_FILES='/fs/pool/pool-mann-projects/Feng/HLA-DB/public_raw/PXD034772/DIA/raw/20200828_DIA_COL011'*.dia
 ## all diann output files will be saved into $OUT_DIR
 OUT_DIR="/fs/pool/pool-mann-projects/Feng/speclib_for_people/MariaW/HLA_personlibs/9/feng/out_panlib"
