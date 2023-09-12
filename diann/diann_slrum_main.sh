674c4a2db3242963e15b70f16f219395bda892b0
jalew188 <jalew.zwf@qq.com>
2023-09-12 10:34:21 +0300

    update usage

--- diann/diann_slrum_main.sh
+++ diann/diann_slrum_main.sh
@@ -10,11 +10,11 @@ _wait="--wait"
 ############## how to use this script #####################
 ### 1. login hpcl: ssh -l your_user_name -X hpcl7001
 ### 
-### 2. copy the script folder to your_pool_folder on windows or use: copy -r /fs/pool/pool-mann-pub/User/Feng/diann_linux/slurm_scripts/* your_pool_folder
+### 2. copy the script folder to your_pool_folder on windows or use: copy -r /fs/pool/pool-mann-pub/User/Feng/diann_linux/slurm_scripts/diann/* your_pool_folder
 ### 
 ### 3. cd your_pool_folder
 ### 
-### 4. sh run_full_workflow.sh to run jobs
+### 4. sh diann_slurm_main.sh to run jobs
 
 ### [5] check job ids in the queue: "squeue" or "squeue -u {username}"
 ### [6] cancel a job: scancel your_job_id
